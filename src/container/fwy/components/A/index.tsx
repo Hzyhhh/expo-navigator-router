ee5e60a877c2345f40ee7e715121209c7644c718
Hzyhhh <799309084@qq.com>
2021-03-19 20:00:23 +0800

    feat: [components] ...

---
+++ src/container/fwy/components/A/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export interface AProps {}
+
+const A: React.FC<AProps> = () => {
+  return <View></View>;
+};
+
+export default A;
---
+++ src/container/fwy/components/index.ts
@@ -0,0 +1,4 @@
+// 业务Components全局导出
+import A from "./A";
+
+export { A };
